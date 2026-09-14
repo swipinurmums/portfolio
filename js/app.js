ed1fe39d8d25860a27a4c2e30a65eb07e618fbad
aronbelle <160137976+swipinurmums@users.noreply.github.com>
2026-09-14 22:41:35 +0100

    stop ios hold save

--- js/app.js
+++ js/app.js
@@ -305,7 +305,14 @@ async function start() {
 }
 
 start();
+
 document.addEventListener("contextmenu", event => {
+  if (event.target.closest(".shot, .shot img")) {
+    event.preventDefault();
+  }
+});
+
+document.addEventListener("dragstart", event => {
   if (event.target.closest(".shot, .shot img")) {
     event.preventDefault();
   }
