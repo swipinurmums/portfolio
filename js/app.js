85153c13815014d4f520280849b4f672af521efe
aronbelle <160137976+swipinurmums@users.noreply.github.com>
2026-09-14 23:06:51 +0100

    wsedfg

--- js/app.js
+++ js/app.js
@@ -305,3 +305,9 @@ async function start() {
 }
 
 start();
+
+document.addEventListener("contextmenu", event => {
+  if (event.target.closest(".shot, .shot img")) {
+    event.preventDefault();
+  }
+});
